cb402d62a29f078bb17a7d036b73e20344ed05e5
thuyngocmaithy <thuyngocmaithyy@gmail.com>
2024-03-13 21:10:03 +0700

    login

--- app/src/main/java/com/example/music_mobile_app/LoginActivity.java
+++ app/src/main/java/com/example/music_mobile_app/LoginActivity.java
@@ -1,50 +1,111 @@
 package com.example.music_mobile_app;
 
-import android.app.Activity;
-import android.graphics.drawable.ColorDrawable;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Gravity;
-import android.view.MenuItem;
+import android.util.Log;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
 
-import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.ContextCompat;
-import androidx.fragment.app.FragmentManager;
 
-import com.example.music_mobile_app.ui.MainFragment;
+import com.spotify.sdk.android.authentication.AuthenticationClient;
+import com.spotify.sdk.android.authentication.AuthenticationRequest;
+import com.spotify.sdk.android.authentication.AuthenticationResponse;
+
+import java.util.Objects;
 
 public class LoginActivity extends AppCompatActivity {
+
+
+    @SuppressWarnings("SpellCheckingInspection")
+    public static final String CLIENT_ID = "5de6930c8a744270851a5064c7ff6333";
+    @SuppressWarnings("SpellCheckingInspection")
+    private static final String REDIRECT_URI = "http://localhost:8888/callback";
+
+    private static final String TAG = "Spotify " + LoginActivity.class.getSimpleName();
+
+    private static final int REQUEST_CODE = 1337;
+
+    public static final String AUTH_TOKEN = "AUTH_TOKEN";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.acitvity_login);
+        // Hide action bar
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        Objects.requireNonNull(getSupportActionBar()).hide();
 
-        // Căn giữa tiêu đề
-        getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
-        android.view.View customView = getLayoutInflater().inflate(R.layout.custom_actionbar_title, null);
-        ActionBar.LayoutParams params = new ActionBar.LayoutParams(
-                ActionBar.LayoutParams.WRAP_CONTENT,
-                ActionBar.LayoutParams.MATCH_PARENT,
-                Gravity.CENTER);
-        getSupportActionBar().setCustomView(customView, params);
-        getSupportActionBar().setDisplayShowCustomEnabled(true);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(ContextCompat.getColor(this, R.color.colorPrimary)));
+        setContentView(R.layout.activity_login);
+
+        Button mLoginButton = (Button)findViewById(R.id.buttonStart);
+        mLoginButton.setOnClickListener(mListener);
 
     }
 
-    // Xử lý sự kiện khi nút quay lại được nhấn
+
+    private void openLoginWindow() {
+
+        AuthenticationRequest.Builder builder = new AuthenticationRequest.Builder(CLIENT_ID, AuthenticationResponse.Type.TOKEN,REDIRECT_URI);
+
+        builder.setScopes(new String[]{"user-read-private", "streaming", "user-top-read", "user-read-recently-played"});
+        AuthenticationRequest request = builder.build();
+
+        AuthenticationClient.openLoginActivity(this, REQUEST_CODE, request);
+    }
+
+
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case android.R.id.home:
-                finish();
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
+    protected void onActivityResult(int requestCode, final int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == REQUEST_CODE)
+        {
+            final AuthenticationResponse response = AuthenticationClient.getResponse(resultCode, data);
+
+            switch (response.getType()) {
+                // Response was successful and contains auth token
+                case TOKEN:
+
+                    Intent intent = new Intent(LoginActivity.this,
+                            MainActivity.class);
+
+                    intent.putExtra(AUTH_TOKEN, response.getAccessToken());
+
+                    startActivity(intent);
+
+                    destroy();
+
+                    break;
+
+                // Auth flow returned an error
+                case ERROR:
+                    Log.e(TAG,"Auth error: " + response.getError());
+                    break;
+
+                // Most likely auth flow was cancelled
+                default:
+                    Log.d(TAG,"Auth result: " + response.getType());
+            }
         }
+    }
+
+    View.OnClickListener mListener = new View.OnClickListener(){
+
+        @Override
+        public void onClick(View view) {
+            switch (view.getId()){
+                case R.id.buttonStart:
+                    openLoginWindow();
+                    break;
+            }
+        }
+    };
+
+    public void destroy(){
+        LoginActivity.this.finish();
     }
 }
